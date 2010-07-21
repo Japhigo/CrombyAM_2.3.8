7c792edd9abb90b53fa6ba55ba170c7696249093
Phil Goldsbrough <phil.goldsbrough@btinternet.com>
2010-07-21 20:01:21 +0100

    Try ref daya validation in a mixin

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -8,48 +8,6 @@ class Gender < ActiveRecord::Base
   
   belongs_to :data_status, :foreign_key => "data_status_code"
   
-  @@ref_data_table = RefDataTable.find(:first, :conditions => "table_name = 'genders'")
-
-  validates_presence_of :code, :if => :code_mandatory?
-  validates_length_of :code, :maximum => 10, :if => :code?
-  validates_uniqueness_of :code, :if => :code?
-  validate :code_is_right_length
-  validate :code_is_right_format
-
-  validates_presence_of :display
-  validates_length_of :display, :maximum => 30
-  validates_uniqueness_of :display
-
-  validates_presence_of :description, :if => :description_mandatory?
-
-protected
-
-  def code_is_right_length
-    errors.add(:code, "Code must be between " << @@ref_data_table.code_min_length.to_s << " and " << @@ref_data_table.code_max_length.to_s ) if
-      self.code.to_s.size < @@ref_data_table.code_min_length or self.code.to_s.size > @@ref_data_table.code_max_length
-  end
-
-  def code_is_right_format
-    if @@ref_data_table.code_format
-      errors.add(:code, "Code must be in the right format: " << @@ref_data_table.code_format) if
-        self.code !~ Regexp.new(@@ref_data_table.code_format)
-    end
-  end
-  
-  def code_mandatory?
-    return @@ref_data_table.code_mandatory
-  end
-
-  def display_mandatory?
-    return @@ref_data_table.display_mandatory
-  end
-
-  def description_mandatory?
-    return @@ref_data_table.description_mandatory
-  end
-
-  def ref_data_code_format
-    return @@ref_data_table.code_format
-  end
+  @ref_data_table = RefDataTable.find_by_table_name("genders")
 
 end
--- lib/cromby_ref_data.rb
+++ lib/cromby_ref_data.rb
@@ -1,11 +1,70 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-
 module CrombyRefData
 
-  def redundant
-    self.available = false
-    self.data_status_code = -1
+  def self.included(base)
+    base.extend(ClassMethods)
+    base.send(:include, InstanceMethods)
+    base.send(:include, Validations)
   end
-  
+
+  module ClassMethods
+  end
+
+  module Validations
+    def self.included(base)
+      base.class_eval do
+
+        validates_presence_of :code, :if => :code_mandatory?
+        validates_length_of :code, :maximum => 10, :if => :code?
+        validates_uniqueness_of :code, :if => :code?
+        validate :code_is_right_length
+        validate :code_is_right_format
+
+        validates_presence_of :display
+        validates_length_of :display, :maximum => 30
+        validates_uniqueness_of :display
+
+        validates_presence_of :description, :if => :description_mandatory?
+
+      end
+    end
+  end
+
+  module InstanceMethods
+
+    def redundant
+      self.available = false
+      self.data_status_code = -1
+    end
+
+  protected
+
+    def code_is_right_length
+      errors.add(:code, "Code must be between " << @ref_data_table.code_min_length.to_s << " and " << @ref_data_table.code_max_length.to_s ) if
+        self.code.to_s.size < @ref_data_table.code_min_length or self.code.to_s.size > @ref_data_table.code_max_length
+    end
+
+    def code_is_right_format
+      if @ref_data_table.code_format
+        errors.add(:code, "Code must be in the right format: " << @ref_data_table.code_format) if
+          self.code !~ Regexp.new(@ref_data_table.code_format)
+      end
+    end
+
+    def code_mandatory?
+      return @ref_data_table.code_mandatory
+    end
+
+    def display_mandatory?
+      return @ref_data_table.display_mandatory
+    end
+
+    def description_mandatory?
+      return @ref_data_table.description_mandatory
+    end
+
+    def ref_data_code_format
+      return @ref_data_table.code_format
+    end
+  end
+
 end
